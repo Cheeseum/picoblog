f9e1aa097ea42b1de552c77229733c8848cb9ee4
Cheeseum <z.cheeseum@gmail.com>
2016-01-01 23:43:45 -0700

    working url namespacing
    * might not be the _best_ way but its okay

--- picoblog/urls.py
+++ picoblog/urls.py
@@ -3,10 +3,13 @@ from django.contrib import admin
 from . import views
 
 app_name = 'picoblog'
-urlpatterns = patterns('',
-
+blogpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^p/(?P<pk>[0-9]+)/$', views.PostView.as_view(), name='post'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.PostView.as_view(), name='post'),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+]
+
+urlpatterns = [
+    url(r'', include((blogpatterns, app_name, app_name)))
+]
