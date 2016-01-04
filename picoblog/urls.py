7b6ee4521071b1e9d9f0cee63b2e322a95fef32e
Cheeseum <z.cheeseum@gmail.com>
2016-01-04 12:49:37 -0700

    fix admin url

--- picoblog/urls.py
+++ picoblog/urls.py
@@ -6,10 +6,9 @@ app_name = 'picoblog'
 blogpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.PostView.as_view(), name='post'),
-
-    url(r'^admin/', include(admin.site.urls)),
 ]
 
 urlpatterns = [
-    url(r'', include((blogpatterns, app_name, app_name)))
+    url(r'', include((blogpatterns, app_name, app_name))),
+    url(r'^admin/', include(admin.site.urls))
 ]
